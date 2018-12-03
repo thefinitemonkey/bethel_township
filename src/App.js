affae6a83c83b1dba63e2039f9e40005e938bde7
Doug Brown <doug.brown@academy.com>
2018-12-03 11:18:11 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {withStyles} from '@material-ui/core/styles';
+//import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
 
+const styles = {
+  bodyLayout: {
+    margin: 15
+  },
+  calendarDisplay: {
+    border: 0,
+    width: 800,
+    height: 600,
+    frameborder: 0,
+    scrolling: "no"
+  }
+}
 class App extends Component {
   render() {
+    const {classes} = this.props;
+    console.log("classes: ", classes);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.bodyLayout}>
+        <h1>Bethel Township, Clark County, OH</h1>
+        <p>Our site is currently undergoing maintenance. In the meantime, calendar
+          information for the availability of Styer and Dennelsville park facilities can
+          be viewed below.</p>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=styer.park%40gmail.com&ctz=America%2FNew_York"
+          className={classes.calendarDisplay}></iframe>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
